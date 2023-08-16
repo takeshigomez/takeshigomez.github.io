18195e63a4b0824c0600bddcca104eeb2bf023e4
Takeshi Gomez <takegomez03@gmail.com>
2023-08-15 22:35:46 -0300

    Pagina

---
+++ workspace-inicial/js/products.js
@@ -0,0 +1,24 @@
+const DATA_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+
+const container = document.getElementById("a")
+async function getJsonData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error fetching data from ${url}: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(`Ha ocurrido un error: ${error.message}`);
+    }
+}
+async function imprimirProductos() {
+    const autos = await getJsonData(DATA_URL);
+    const products = autos.products
+    products.forEach(auto => {
+        container.innerHTML += `<div><h1>&raquo; MODELO: ${auto.name}</h1>  <p> &raquo; Costo: ${auto.cost}</p> <p>&raquo; Vendidos: ${auto.soldCount}</p> <img src="${auto.image}" style=""> </div>`
+    });
+
+}
+imprimirProductos();
