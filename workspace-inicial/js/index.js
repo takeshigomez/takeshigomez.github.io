d4159a4e9750f003f844459279a9def5ea03a686
RodrigoPrunell26 <rodrigoprunell26@hotmail.com>
2023-08-17 18:55:50 -0300

    Revert "DESAFIATE"
    This reverts commit 6146939bbba3b99c4cd192bbff45661050e894ba.

--- workspace-inicial/js/index.js
+++ workspace-inicial/js/index.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-let logueo = sessionStorage.getItem("user");
-
-    if(logueo == null)
-{
-    location.href = "./login.html";
-}
-
-
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
