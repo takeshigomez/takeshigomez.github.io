55b7392dd9bd033af981eb310e2401f3514e9460
toongass <119079321+toongass@users.noreply.github.com>
2023-09-02 12:00:23 -0300

    ultimos cambios

--- workspace-inicial/js/products.js
+++ workspace-inicial/js/products.js
@@ -1,6 +1,13 @@
+/* S9.2: Cuando el usuario selecciona una categoría de productos, su identificador es guardado en el almacenamiento local antes de redirigir
+ a productos.html.
+Modifica la solicitud realizada en la carga del listado de productos (que hicimos en la entrega anterior) 
+para que utilice ese identificador, en lugar de 101. */
+
 let categoria = localStorage.getItem("catID");
 const DATA_URL = PRODUCTS_URL + categoria + EXT_TYPE;
 const container = document.getElementById("cargaProductos");
+
+
 // se cambia productos por autos ya que esto se utiliza en imprimirProductos()
 let autos;                                                               
 
@@ -32,12 +39,23 @@ if (logueado == null) {
     location.href= "./login.html";
 }
 
+/* S9.1 : En la barra de navegación superior, agregar en la esquina derecha el 
+nombre del usuario ingresado en la pantalla de inicio de sesión.
+ Para ello deberás hacer uso del almacenamiento local. */
+
 const pcontainer = document.getElementById("perfil")
 pcontainer.innerHTML += logueado
 
 
+
+/* S9.3 Con el listado de productos desplegado:
+Aplicar filtros a partir de rango de precio definido.
+Agregar las funcionalidades de orden ascendente y descendente en función del precio y 
+descendente en función de la relevancia (tomaremos para ello la cantidad de artículos vendidos) */
+
+// Funcion de ordenamiento
 function precioASD(a, b){
-    return a.cost - b.cost;
+    return a.cost - b.cost; 
 }
 function precioDSN(a, b){
     return b.cost - a.cost;
@@ -62,7 +80,7 @@ function ordenarRelevancia() {
     actualizarLista();
 }
 
-///////////////////
+// FUNCION DE ACTUALIZAR LISTA PARA EL FILTRO CORRESPONDIENTE
 function actualizarLista(filtro = '', precioMin = 0, precioMax = Infinity) {
     const contenedor = document.getElementById("cargaProductos");
     const resultadoBusqueda = document.getElementById('resultadoBusqueda');
@@ -89,23 +107,15 @@ function actualizarLista(filtro = '', precioMin = 0, precioMax = Infinity) {
                 </div>`;
         }
     });
-    
-  //  if (resultadoBusqueda.children.length === 0) {
-    //    const li = document.createElement('li');
-      //  li.textContent = "No se encontraron resultados.";
-      //  resultadoBusqueda.appendChild(li);
-    //}
 }
 
-////////////////////
-
-
+//CONSTANTES PARA LOS EVENTOS
 const campoBusqueda = document.getElementById('buscar');
 const campoPrecioMin = document.getElementById('rangeFilterCountMin');
 const campoPrecioMax = document.getElementById('rangeFilterCountMax');
 const limpiar = document.getElementById('clearRangeFilter');
 
-//////////////////
+// NUEVA FUNCION DE CARGO AUTOS PARA EL LIMPIAR
 function cargarTodosLosAutos() {
     const contenedor = document.getElementById("cargaProductos");
     contenedor.innerHTML = ''; // Limpiar el contenido actual
@@ -125,15 +135,13 @@ function cargarTodosLosAutos() {
     });
 }
 
+//EVENTOS DE LOS BOTONES DE ORDENAR ,FILTRAR Y LIMPIAR
 document.getElementById('clearRangeFilter').addEventListener('click', () => {
     campoBusqueda.value = ''; // Limpiar el campo de búsqueda
     campoPrecioMin.value = 0; // Restablecer el rango de precios mínimo
     campoPrecioMax.value = ''; // Restablecer el rango de precios máximo
     cargarTodosLosAutos(); // Cargar todos los autos sin filtro
 });
-
-
-/////////////1
 
 campoBusqueda.addEventListener('input', () => {
     const filtro = campoBusqueda.value;
