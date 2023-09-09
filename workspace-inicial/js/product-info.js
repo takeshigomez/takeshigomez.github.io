af086b68cffdd45b5e5c08b56b79e12fb34bf660
RodrigoPrunell26 <rodrigoprunell26@hotmail.com>
2023-09-09 17:26:26 -0300

    UltimoHecho

--- workspace-inicial/js/product-info.js
+++ workspace-inicial/js/product-info.js
@@ -2,6 +2,7 @@ let producto = localStorage.getItem("prodID");
 const ARTICULO_URL = PRODUCT_INFO_URL + producto + EXT_TYPE;
 const container = document.getElementById("cargaProductos");
 
+let articulos;
 async function getJsonData(url) {
     try {
         const response = await fetch(url);
@@ -15,11 +16,19 @@ async function getJsonData(url) {
     }
 }
 async function imprimirArticulo() {
-    artitculos = await getJsonData(ARTICULO_URL);
-    const products = artitculos.products
-    products.forEach(articulo => {
-        container.innerHTML += `<div id="productos" onclick="setProdID(${articulo.id})"> <div><ul><h1>${articulo.name}</h1>   <p>${articulo.description}</p> <p>${articulo.currency} ${articulo.cost}</p> <p>${articulo.soldCount}</p></ul></div> <img src="${articulo.image}"></div>`
-    });
+    articulos = await getJsonData(ARTICULO_URL);
+    for (let i = 0; i < articulos.length; i++) {
+        const product = articulos;
+        container.innerHTML += `<div(${product.id})">
+            <div>
+                <ul>
+                    <h1>${product.name}</h1>
+                    
+                </ul>
+            </div>
+           
+        </div>`;
+    };
 }
 imprimirArticulo();
 
