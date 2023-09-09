7352f86db8e8c9c183ab7ca4ce24bd5e71ef233f
Takeshi Gomez <takegomez03@gmail.com>
2023-09-09 19:08:52 -0300

    S11 P3 inProgress

--- workspace-inicial/js/product-info.js
+++ workspace-inicial/js/product-info.js
@@ -12,7 +12,7 @@ if (producto) {
             // Maneja la respuesta de la API y muestra la información del producto
             const container = document.getElementById("cargaProductos");
             container.innerHTML = `
-                <div id="aDescripcion">
+                <div>
                 <ul>
                     <li><h1>${data.name}</h1></li>
                     <li><p>${data.description}</p></li>
@@ -25,6 +25,39 @@ if (producto) {
                 container.innerHTML += `<div id="articulos"> <img src= "${element[i]}"> </div>`
                 
             }
+        })
+        .catch(error => {
+            console.error('Error al obtener la información del producto:', error);
+            // Maneja el error de la solicitud
+        });
+} else {
+    // Maneja el caso en el que no se haya seleccionado ningún producto.
+    document.write('Ningún producto seleccionado.');
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+if (producto) {
+    // Construye la URL de la API con el identificador del producto
+    const COMENTARIO_URL = PRODUCT_INFO_COMMENTS_URL + producto + EXT_TYPE;
+    // Realiza una solicitud GET a la API
+    fetch(COMENTARIO_URL)
+        .then(response => response.json())
+        .then(data => {
+            // Maneja la respuesta de la API y muestra la información del producto
+            const container = document.getElementById("comentariosProductos");
+            container.innerHTML += `<h1>COMENTARIO</h1>`
+            for (let i = 0; i < data.length; i++) {
+                container.innerHTML += `
+                <div>
+                <ul id ="articuloComentarios">
+                    <li><p>${data[i].user}</p></li>
+                    <li><p>${data[i].dateTime}</p></li>
+                    <li><p>${data[i].description}</p></li>
+                </ul>
+            </div>`
+                
+            }
+                            
         })
         .catch(error => {
             console.error('Error al obtener la información del producto:', error);
