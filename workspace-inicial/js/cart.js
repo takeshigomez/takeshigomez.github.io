de8447074882e510784818c356023e602669d7ca
gonzalosantoro <gonzalosantoro07@gmail.com>
2023-10-18 19:10:11 -0300

    pauta 1 y 2 entrega 6

--- workspace-inicial/js/cart.js
+++ workspace-inicial/js/cart.js
@@ -1,15 +1,9 @@
-
 // Función para cargar y mostrar los datos del carrito
 function loadCartData() {
   const cartUrl = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
 
   fetch(cartUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos del carrito de compras.');
-      }
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
       const productList = document.getElementById('product-list');
 
@@ -32,7 +26,7 @@ function loadCartData() {
           <tbody>
           </tbody>
           </div>`;
-        
+
         const tbody = table.querySelector('tbody');
         data.articles.forEach(article => {
           // Crear una fila para cada artículo
@@ -42,27 +36,27 @@ function loadCartData() {
             <td><img class="img-thumbnail" style="max-width: 100px;" src="${article.image}" alt="${article.name}"></td>
             <td><p>${article.name}</p></td>
             <td><p class="product-cost">${article.currency} ${article.unitCost}</p></td>
-            <td><input class="form-control form-control-sm quantity-input" type="number" id="cantidad" value="${article.count}"></td>
-            <td><p class="product-info product-subtotal">${article.unitCost} ${article.currency}</p></td>
+            <td><input class="form-control form-control-sm quantity-input" type="number" value="${article.count}"></td>
+            <td><p class="product-info product-subtotal">${article.currency} ${article.unitCost * article.count}</p></td>
           `;
           // Agregar la fila a la tabla
           tbody.appendChild(tr);
+
           // Agregar event listener para actualizar el subtotal en tiempo real al modificar la cantidad
           const quantityInput = tr.querySelector('.quantity-input');
-          quantityInput.addEventListener('input', function () {
-            // Obtener el nuevo valor de la cantidad
-            const newQuantity = parseInt(this.value, 10);
-            // Validar que la nueva cantidad sea un número válido
-            if (!isNaN(newQuantity) && newQuantity >= 0) {
-              // Actualizar el subtotal en función de la nueva cantidad
-              const newSubtotal = article.unitCost * newQuantity;
-              tr.querySelector('.product-subtotal').textContent = `${newSubtotal} ${article.currency}`;
-            }
-          });
+          quantityInput.addEventListener('input', updateProductSubtotal);
         });
-        
+
         // Agregar la tabla completa al contenedor
         productList.appendChild(table);
+
+        // Agregar event listener para los radios de envío
+        const shippingRadios = document.querySelectorAll('input[type="radio"][name="shipping"]');
+        shippingRadios.forEach(radio => {
+          radio.addEventListener('change', updateSummary);
+        });
+
+        updateSummary(); // Calcular el resumen inicial
       } else {
         console.error('El formato de los datos del carrito es incorrecto.');
       }
@@ -70,6 +64,82 @@ function loadCartData() {
     .catch(error => {
       console.error('Error al obtener los datos del carrito de compras:', error);
     });
+
+  // Función para actualizar el subtotal del producto en tiempo real
+  function updateProductSubtotal() {
+    const tr = this.closest('tr'); // Obtener la fila que contiene el campo de cantidad
+    const quantity = parseInt(this.value, 10);
+    if (!isNaN(quantity) && quantity >= 0) {
+      const productCostElement = tr.querySelector('.product-cost');
+      const productSubtotalElement = tr.querySelector('.product-subtotal');
+
+      const productCostText = productCostElement.textContent;
+      const [currency, amount] = productCostText.split(' ');
+      const productCost = parseFloat(amount);
+
+      const subtotal = currency + ' ' + (productCost * quantity).toFixed(2);
+      productSubtotalElement.textContent = subtotal;
+      updateSummary(); // Actualizar el resumen de compra
+    }
+  }
+
+  // Función para calcular el resumen de la compra
+  function updateSummary() {
+    const subtotalElements = document.querySelectorAll('.product-subtotal');
+    const subtotalGeneralElement = document.getElementById('subtotal-general');
+    const costoEnvioElement = document.getElementById('costo-envio');
+    const totalPagarElement = document.getElementById('total-pagar');
+
+    let subtotalGeneral = 0;
+    subtotalElements.forEach(subtotalElement => {
+      const subtotalText = subtotalElement.textContent;
+      const [currency, amount] = subtotalText.split(' ');
+      const subtotal = parseFloat(amount);
+      subtotalGeneral += subtotal;
+    });
+
+    subtotalGeneralElement.textContent = `USD ${subtotalGeneral.toFixed(2)}`;
+
+    const selectedShipping = document.querySelector('input[type="radio"][name="shipping"]:checked');
+    if (selectedShipping) {
+      const shippingValue = parseFloat(selectedShipping.value) / 100;
+      const costoEnvio = subtotalGeneral * shippingValue;
+      costoEnvioElement.textContent = `USD ${costoEnvio.toFixed(2)}`;
+
+      const totalPagar = subtotalGeneral + costoEnvio;
+      totalPagarElement.textContent = `USD ${totalPagar.toFixed(2)}`;
+    } else {
+      costoEnvioElement.textContent = `USD 0.00`;
+      totalPagarElement.textContent = `USD ${subtotalGeneral.toFixed(2)}`;
+    }
+  }
 }
+
 // Llamar a la función cuando la página se carga completamente
-window.addEventListener('load', loadCartData);  
+window.addEventListener('load', loadCartData);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const paymentModal = new bootstrap.Modal(document.getElementById('paymentModal'));
+  const paymentMethodRadios = document.querySelectorAll('input[name="paymentMethod"]');
+  const creditCardForm = document.getElementById('creditCardForm');
+  const bankTransferForm = document.getElementById('bankTransferForm');
+  const confirmPaymentButton = document.getElementById('confirmPayment');
+
+  // Event listener para mostrar u ocultar los formularios según la selección
+  paymentMethodRadios.forEach(radio => {
+    radio.addEventListener('change', function () {
+      if (this.value === 'creditCard') {
+        creditCardForm.style.display = 'block';
+        bankTransferForm.style.display = 'none';
+      } else {
+        creditCardForm.style.display = 'none';
+        bankTransferForm.style.display = 'block';
+      }
+    });
+  });
+
+  // Event listener para abrir el modal
+  confirmPaymentButton.addEventListener('click', function () {
+    paymentModal.show();
+  });
+});
