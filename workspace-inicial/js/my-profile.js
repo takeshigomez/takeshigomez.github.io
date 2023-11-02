cda8257411e8b935a7b6baaaff48951bb988925c
RodrigoPrunell26 <rodrigoprunell26@hotmail.com>
2023-11-02 18:07:41 -0300

    imagen por defecto e imagen seleccionada

--- workspace-inicial/js/my-profile.js
+++ workspace-inicial/js/my-profile.js
@@ -18,6 +18,21 @@ const menuDropdown = document.getElementById("divDropdown");
 const toggleDropdown = function () {
   menuDropdown.classList.toggle("show"); // toggle modifica la clase
 };
+document.getElementById("archivo").addEventListener("change", function() {
+  const imagenPerfil = document.getElementById("imagenPerfil");
+  const fileInput = this; // "this" se refiere al input de archivo
+
+  if (fileInput.files && fileInput.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      // Establecer el atributo src de la imagen con la URL de la imagen cargada
+      imagenPerfil.src = e.target.result;
+    };
+
+    reader.readAsDataURL(fileInput.files[0]);
+  }
+});
 
 botonDropdown.addEventListener("click", function (e) {
   e.stopPropagation();
